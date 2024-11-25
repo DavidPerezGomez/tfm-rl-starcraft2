33949db27e57210baa21def4435c0df190ca6d77
David Pérez Gómez <davidperezgo@gmail.com>
2024-11-25 19:28:32 +0100

    Prevent attack manager from choosing NO-OP

--- src/tfm_sc2/agents/multi/army_attack_manager_dqn_agent.py
+++ src/tfm_sc2/agents/multi/army_attack_manager_dqn_agent.py
@@ -1,6 +1,19 @@
+from pysc2.env.environment import TimeStep
+from typing_extensions import override
+from typing import List
+
 from ..dqn_agent import DQNAgent
 from .with_army_attack_manager_actions import WithArmyAttackManagerActions
+from ...actions import AllActions
 
 
 class ArmyAttackManagerDQNAgent(WithArmyAttackManagerActions, DQNAgent):
-    pass
+
+    @override
+    def available_actions(self, obs: TimeStep) -> List[AllActions]:
+        available_actions = super().available_actions(obs)
+
+        if len(available_actions) > 1 and AllActions.NO_OP in available_actions:
+            available_actions = [a for a in available_actions if a != AllActions.NO_OP]
+
+        return available_actions
--- src/tfm_sc2/agents/multi/army_attack_manager_random_agent.py
+++ src/tfm_sc2/agents/multi/army_attack_manager_random_agent.py
@@ -1,7 +1,20 @@
+from pysc2.env.environment import TimeStep
+from typing_extensions import override
+from typing import List
+
 from ..base_agent import BaseAgent
 from .with_army_attack_manager_actions import WithArmyAttackManagerActions
 from .with_random_action_selection import WithRandomActionSelection
+from ...actions import AllActions
 
 
 class ArmyAttackManagerRandomAgent(WithArmyAttackManagerActions, WithRandomActionSelection, BaseAgent):
-    pass
+
+    @override
+    def available_actions(self, obs: TimeStep) -> List[AllActions]:
+        available_actions = super().available_actions(obs)
+
+        if len(available_actions) > 1 and AllActions.NO_OP in available_actions:
+            available_actions = [a for a in available_actions if a != AllActions.NO_OP]
+
+        return available_actions
